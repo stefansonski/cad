2105f859e8de6422ea716268b3cacbcadb916609
Sven Hartz <svenhartz@users.noreply.github.com>
2012-03-28 11:22:53 +0000

    fixed size House

--- haus4/acrxEntryPoint.cpp
+++ haus4/acrxEntryPoint.cpp
@@ -64,7 +64,59 @@ public:
 	static void CGCADhaus4house4(void)
 	{
 		// Add your code for command CGCADhaus4.house4 here
-		acutPrintf(_T("\nHello World!"));
+		//ground
+		AcGePoint3d groundPoint1(0.0, 0.0, 0.0);
+		AcGePoint3d groundPoint2(10.0, 0.0, 0.0);
+		AcGePoint3d groundPoint3(10.0, 10.0, 0.0);
+		AcGePoint3d groundPoint4(0.0, 10.0, 0.0);
+		//ceiling	
+		AcGePoint3d ceilingPoint1(0.0, 0.0, 10.0);
+		AcGePoint3d ceilingPoint2(10.0, 0.0, 10.0);
+		AcGePoint3d ceilingPoint3(10.0, 10.0, 10.0);
+		AcGePoint3d ceilingPoint4(0.0, 10.0, 10.0);
+		//roof
+		AcGePoint3d roofPoint1(5.0, 0.0, 15.0);
+		AcGePoint3d roofPoint2(5.0, 10.0, 15.0);
+
+		//initialize 3dFaces
+		AcDbFace* ground = new AcDbFace(groundPoint1, groundPoint2, groundPoint3, groundPoint4, TRUE, TRUE, TRUE, TRUE);
+		AcDbFace* wall1 = new AcDbFace(groundPoint1, groundPoint2, ceilingPoint2, ceilingPoint1, TRUE, TRUE, FALSE, TRUE);
+		AcDbFace* wall11 = new AcDbFace(ceilingPoint1, ceilingPoint2, roofPoint1, FALSE, TRUE, TRUE, FALSE);
+		AcDbFace* wall2 = new AcDbFace(groundPoint2, groundPoint3, ceilingPoint3, ceilingPoint2, TRUE, TRUE, TRUE, TRUE);
+		AcDbFace* wall3 = new AcDbFace(groundPoint3, groundPoint4, ceilingPoint4, ceilingPoint3, TRUE, TRUE, FALSE, TRUE);
+		AcDbFace* wall31 = new AcDbFace(ceilingPoint3, ceilingPoint4, roofPoint2, FALSE, TRUE, TRUE, FALSE);
+		AcDbFace* wall4 = new AcDbFace(groundPoint4, groundPoint1, ceilingPoint1, ceilingPoint4, TRUE, TRUE, TRUE, TRUE);
+		AcDbFace* roof1 = new AcDbFace(ceilingPoint1, roofPoint1, roofPoint2, ceilingPoint4, TRUE, TRUE, TRUE, TRUE);
+		AcDbFace* roof2 = new AcDbFace(ceilingPoint2, ceilingPoint3, roofPoint2, roofPoint1, TRUE, TRUE, TRUE, TRUE);
+
+		AcDbBlockTable* pBlockTable = NULL;
+
+		AcDbDatabase* pDB = acdbHostApplicationServices()->workingDatabase();
+		pDB->getSymbolTable(pBlockTable, AcDb::kForRead);
+		AcDbBlockTableRecord* pBlockTableRecord = NULL;
+		pBlockTable->getAt(ACDB_MODEL_SPACE, pBlockTableRecord, AcDb::kForWrite);
+		pBlockTable->close();
+
+		pBlockTableRecord->appendAcDbEntity(ground);
+		pBlockTableRecord->appendAcDbEntity(wall1);
+		pBlockTableRecord->appendAcDbEntity(wall11);
+		pBlockTableRecord->appendAcDbEntity(wall2);
+		pBlockTableRecord->appendAcDbEntity(wall3);
+		pBlockTableRecord->appendAcDbEntity(wall31);
+		pBlockTableRecord->appendAcDbEntity(wall4);
+		pBlockTableRecord->appendAcDbEntity(roof1);
+		pBlockTableRecord->appendAcDbEntity(roof2);
+
+		pBlockTableRecord->close();
+		ground->close();
+		wall1->close();
+		wall11->close();
+		wall2->close();
+		wall3->close();
+		wall31->close();
+		wall4->close();
+		roof1->close();
+		roof2->close();
 	}
 } ;
 
