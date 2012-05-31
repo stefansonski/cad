a612e5ac65d439ed4fada415583f6e2bcef34128
Sven Hartz <svenhartz@users.noreply.github.com>
2012-05-31 23:44:54 +0000

    geht nun

--- Praktikum4/acrxEntryPoint.cpp
+++ Praktikum4/acrxEntryPoint.cpp
@@ -24,6 +24,7 @@
 //-----------------------------------------------------------------------------
 #include "StdAfx.h"
 #include "resource.h"
+#include <math.h>
 
 //-----------------------------------------------------------------------------
 #define szRDS _RXST("CGCAD")
@@ -72,7 +73,10 @@ public:
 		filter.resval.rstring = _T("3DFACE");
 		filter.rbnext = NULL;
 
+		// hier kommen die 2 Ebenen rein
 		AcDbFace* faces[2];
+
+		// Filter der nur faces liefert
 		for(int i = 0; i < 2; i++) {
 			ads_name ssname, ent;
 			AcDbObjectId id = AcDbObjectId::kNull;
@@ -87,132 +91,174 @@ public:
 			faces[i] = (AcDbFace*)tmp;
 		}
 
-		AcGePoint3d points[2][4];	
-		AcGeVector3d normal[2];
+		/*-Deklaration-----------------------*/
+												// 2 Ebenenen, 4 Eckpunkte + homogene Koordinate
+		ads_real sFaceCorners[4][4] = {};		// sface = small Face
+		ads_real bFaceCorners[4][4] = {};		// bface = bigface
 
-		for(int i = 0; i < 2; i++)
-		{
-			for(int j = 0; j < 4; j++)
-			{
-				faces[i]->getVertexAt(j, points[i][j]);
-			}
+		ads_real sDirection[2][4] = {};			// Ebenen: 2 Richtungsvektoren à 4 Koordinaten
+		ads_real bDirection[2][4] = {};
 
-			AcGeVector3d direction01;
-			AcGeVector3d direction03;
-			direction01 = points[i][1] - points[i][0];
-			direction03 = points[i][3] - points[i][0];
-			normal[i] = direction01.crossProduct(direction03);
-			normal[i] = normal[i].normalize();
-		}
+		ads_real sNormal[4] = {};				// Normale...
+		ads_real bNormal[4] = {};
 
-		AcGeVector3d crossVec;
-		crossVec = normal[0].crossProduct(normal[1]);
-		crossVec = crossVec.normalize();
 
-		double result[2];
-		double cross1, cross2, crossVec1, crossVec2;
-		for(int i = 0; i < 2; i++) 
-		{
-			result[i] = points[i][0].asVector().negate().dotProduct(crossVec);
-		}
+		/*-Zuweisung Ebene-------------------*/
 
-		if(normal[0].x != 0.0 && (normal[1].x * normal[0].y / normal[0].x) + normal[1].y != 0.0)
-		{
-			cross2 = -1 * ((result[1] - (normal[1].x * result[0] / normal[0].x)) / ((normal[1].x * normal[0].y / normal[0].x) + normal[1].y));
-			cross1 = (result[0] - normal[0].y * cross2) / normal[0].x;
-			crossVec2 = crossVec.y;
-			crossVec1 = crossVec.x;
-		}
-		else if(normal[0].y != 0.0 && (normal[1].y * normal[0].z / normal[0].y) + normal[1].z != 0.0)
-		{
-			cross2 = -1 * ((result[1] - (normal[1].y * result[0] / normal[0].z)) / ((normal[1].y * normal[0].z / normal[0].y) + normal[1].z));
-			cross1 = (result[0] - normal[0].z * cross2) / normal[0].y;
-			crossVec2 = crossVec.x;
-			crossVec1 = crossVec.z;
-		}
-		else if(normal[0].z != 0.0 && (normal[1].z * normal[0].x / normal[0].z) + normal[1].x != 0.0)
-		{
-			cross2 = -1 * ((result[1] - (normal[1].z * result[0] / normal[0].x)) / ((normal[1].z * normal[0].x / normal[0].z) + normal[1].x));
-			cross1 = (result[0] - normal[0].x * cross2) / normal[0].z;
-			crossVec2 = crossVec.y;
-			crossVec1 = crossVec.z;
-		}
+		getFacePoints(faces[0],sFaceCorners);
+		getFacePoints(faces[1],bFaceCorners);
 
-		AcGePoint3d crossPoint[2];
-		int counter = 0;
-		//dirty shit is now comming
-		for(int i = 0; i < 4; i++) {
-			int next = (i + 1) % 4;
-			AcGeVector3d tmp = points[0][next] - points[0][i];
-			boolean found = false;
-			double a1, a2, a3, tmp1, tmp2, tmp3;
-			if(tmp.x != 0.0 && tmp.y != 0.0)
-			{
-				a1 = points[0][i].x;
-				a2 = points[0][i].y;
-				a3 = points[0][i].z;
-				tmp1 = tmp.x;
-				tmp2 = tmp.y;
-				tmp3 = tmp.z;
-				found = true;
-			}
-			else if(tmp.x != 0.0 && tmp.z != 0.0)
-			{
-				a1 = points[0][i].x;
-				a2 = points[0][i].z;
-				a3 = points[0][i].y;
-				tmp1 = tmp.x;
-				tmp2 = tmp.z;
-				tmp3 = tmp.y;
-				found = true;
-			}
-			else if(tmp.y != 0.0 && tmp.z != 0.0) 
-			{
-				a1 = points[0][i].y;
-				a2 = points[0][i].z;
-				a3 = points[0][i].x;
-				tmp1 = tmp.y;
-				tmp2 = tmp.z;
-				tmp3 = tmp.x;
-				found = true;
-			}
+		
+		getFaceDirection(sFaceCorners, sDirection);
+		getFaceDirection(bFaceCorners, bDirection);
 
-			if(found)
-			{
-				double mue = (a2 - cross2 + ((cross1 * tmp2 - a1 * tmp2) / tmp1)) / (crossVec2 - ((crossVec1 * tmp2) / tmp1));
-				double lambda = (cross1 + mue * crossVec1 - a1) / tmp1;
-				crossPoint[counter].x = points[0][i].x + lambda * tmp.x;
-				crossPoint[counter].y = points[0][i].y + lambda * tmp.y;
-				crossPoint[counter].z = points[0][i].z + lambda * tmp.z;
-				counter++;
-				if(counter == 2)
-				{
-					acutPrintf(_T("2 Points found"));
-					break;
-				}
-			}
-		}
+		getNormal(sFaceCorners[0], sDirection, sNormal);
+		getNormal(bFaceCorners[0], bDirection, bNormal);
 
-		counter = 0;
-		for(int i = 0; i < 4; i++)
-		{
-			if((normal[1].dotProduct(points[0][i].asVector()) - result[1])  < 0.0)
-			{
-				if(points[0][i].x == crossPoint[counter].x || points[0][i].y == crossPoint[counter].y || points[0][i].z == crossPoint[counter].z)
-				{
-					faces[0]->setVertexAt(i, crossPoint[counter]);
-					counter++;
-				}
-				else
-					faces[0]->setVertexAt(i, crossPoint[counter+1]);
-			}
-		}
+		getNormalizedNormal(sNormal);
+		getNormalizedNormal(bNormal);
 
+		
+		/*-Auf gehts-------------------------*/
+				
+		int index[2] = {}; 
+		getPointsToChange(bNormal, sFaceCorners, index);	// Welche Punkte müssen geändert werden?
+		
+		int indexDirection = -1;
+		getDirection(index, indexDirection);				// Welcher Richtungsvektor wird benötigt? 
+
+
+		ads_real intersection[4] = {};
+		getIntersection(sFaceCorners[index[0]],sDirection[indexDirection],bNormal,intersection);			// Neuen Punkt ermitteln
+		faces[0]->setVertexAt(index[0],AcGePoint3d (intersection[0],intersection[1],intersection[2]));		// Neuen Punkt setzen
+
+		getIntersection(sFaceCorners[index[1]],sDirection[indexDirection],bNormal,intersection);			// Neuen Punkt ermitteln
+		faces[0]->setVertexAt(index[1],AcGePoint3d (intersection[0],intersection[1],intersection[2]));		// Neuen Punkt setzen
+
+		
 		for(int i = 0; i < 2; i++) {
 			faces[i]->close();
 		}
 		reactivateSnap(&oldOsnap);
 	}
+	
+	
+	
+	static void getDirection(int (&index)[2], int (&indexDirection)){
+		
+		if(index[0] + index[1] == 3){		// Index addiert == 3 -> strecke [0]:[1] bzw [2]:[3], 
+			indexDirection = 0;				// damit direction 0
+		} else {							// direction 0 zeigt weg von 0 und zeigt richtung 1 
+			indexDirection = 1;				// direction 1 zeigt weg von 0 und zeigt richtung 3 
+		}
+
+	}
+
+	static void getIntersection(ads_real (&refPoint)[4], ads_real (&direction)[4], ads_real (&normal)[4], ads_real (&intersection)[4]){
+		
+		ads_real result = 0;
+		ads_real top = (normal[0] * refPoint[0]) + (normal[1] * refPoint[1]) + (normal[2] * refPoint[2]) + (normal[3] * refPoint[3]);
+		
+		// wenn kein schnittpunkt wird down = 0
+		ads_real down = (normal[0] * direction[0]) + (normal[1] * direction[1]) + (normal[2] * direction[2]) + (normal[3] * direction[3]);
+
+		result = (-1) * (top / down); 
+
+		intersection[0] = refPoint[0] + result * direction[0];
+		intersection[1] = refPoint[1] + result * direction[1];
+		intersection[2] = refPoint[2] + result * direction[2];
+		intersection[3] = refPoint[3] + result * direction[3];
+	}
+
+	static void getPointsToChange(ads_real (&bNormal)[4], ads_real (&sFaceCorners)[4][4], int (&index)[2]){	
+
+		for(int i= 0; i < 4; i++){
+
+			ads_real firstPoint = bNormal[0] * sFaceCorners[i%4][0] + bNormal[1] * sFaceCorners[i%4][1] + bNormal[2] * sFaceCorners[i%4][2] + bNormal[3] * sFaceCorners[i%4][3];
+			ads_real secondPoint = bNormal[0] * sFaceCorners[(i+1)%4][0] + bNormal[1] * sFaceCorners[(i+1)%4][1] + bNormal[2] * sFaceCorners[(i+1)%4][2] + bNormal[3] * sFaceCorners[(i+1)%4][3];
+			
+			ads_real lot[4] = {};
+			ads_real lotDirection = (-1) *(firstPoint / (bNormal[0] * bNormal[0] + bNormal[1] * bNormal[1] + bNormal[2] * bNormal[2] + bNormal[3] * bNormal[3]));
+
+			lot[0] = sFaceCorners[i][0] + (lotDirection * bNormal[0]);
+			lot[1] = sFaceCorners[i][1] + (lotDirection * bNormal[1]);
+			lot[2] = sFaceCorners[i][2] + (lotDirection * bNormal[2]);
+			lot[3] = sFaceCorners[i][3] + (lotDirection * bNormal[3]);
+
+			if(firstPoint * secondPoint > 0 && sFaceCorners[i][2] < lot[2]){
+				index[0] = i;
+				index[1] = (i+1)%4;
+				return;
+			}
+		}
+
+		/*int counter = 0;
+
+		for(int i=0; i < 4; i++){
+			
+			ads_real pointIs = bNormal[0] * sFaceCorners[i][0] + bNormal[1] * sFaceCorners[i][1] + bNormal[2] * sFaceCorners[i][2] + bNormal[3] * sFaceCorners[i][3];
+			
+			if(pointIs < 0){			// liegt unterhalb der größeren Ebene
+				index[counter++] = i;
+			}
+		}*/
+	}
+
+	static void getNormalizedNormal(ads_real (&normal)[4]) {
+		
+		ads_real length = sqrt (normal[0] * normal[0] + normal[1] * normal[1] + normal[2] * normal[2] + normal[3] * normal[3]);
+
+		if(length == 0) {return;}
+
+		normal[0] = normal[0] / length;
+		normal[1] = normal[1] / length;
+		normal[2] = normal[2] / length;
+		normal[3] = normal[3] / length;
+
+	}
+
+	static void getFaceDirection(ads_real (&facePoints)[4][4], ads_real (&direction)[2][4]) {
+		
+		// 1. Richtungsvektor zeigt von [0][0] nach [1][0] 
+		direction[0][0] = ((-1) * facePoints[0][0]) + facePoints[1][0];
+		direction[0][1] = ((-1) * facePoints[0][1]) + facePoints[1][1];
+		direction[0][2] = ((-1) * facePoints[0][2]) + facePoints[1][2];
+		direction[0][3] = ((-1) * facePoints[0][3]) + facePoints[1][3];
+
+		// 2. Richtungsvektor zeigt von [0][0] nach [3][0]
+		direction[1][0] = ((-1) * facePoints[0][0]) + facePoints[3][0];
+		direction[1][1] = ((-1) * facePoints[0][1]) + facePoints[3][1];
+		direction[1][2] = ((-1) * facePoints[0][2]) + facePoints[3][2];
+		direction[1][3] = ((-1) * facePoints[0][3]) + facePoints[3][3];
+	}
+
+	static void getNormal(ads_real (&refPoint)[4], ads_real (&direction)[2][4], ads_real (&normal)[4]) {
+		
+		normal[0] = ((direction[0][1] * direction[1][2]) - (direction[0][2] * direction[1][1]));
+		
+		normal[1] = ((direction[0][0] * direction[1][2]) - (direction[0][2] * direction[1][0])) * (-1);
+		
+		normal[2] = ((direction[0][0] * direction[1][1]) - (direction[0][1] * direction[1][0]));
+		
+		normal[3] = (-1) * (refPoint[0] * ((direction[0][1] * direction[1][2]) - (direction[0][2] * direction[1][1]))) - (refPoint[1] * ((direction[0][0] * direction[1][2]) - (direction[0][2] * direction[1][0]))) + (refPoint[2] * ((direction[0][0] * direction[1][1]) - (direction[0][1] * direction[1][0])));
+	}
+
+	static void getFacePoints(AcDbFace*& face, ads_real (&facePoints)[4][4]) {
+		
+		AcGePoint3d points3d[4];
+		
+		for(int i = 0; i < 4; i++){		// 4 Eckpunkte
+					
+			face->getVertexAt(i, points3d[i]);	// zuweisung zu points3d
+
+			facePoints[i][0] = points3d[i].x; 
+			facePoints[i][1] = points3d[i].y; 
+			facePoints[i][2] = points3d[i].z; 
+			facePoints[i][3] = 1;					// setze homohene koordinaten = 1
+		}	
+	}
+
+
 
 	static void deactivateSnap(resbuf* oldOsnap) {
 		struct resbuf newOsnap;
