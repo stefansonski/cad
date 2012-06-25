5d38e6e658f168d7f6410fc5a808978cde1d474e
Sven Hartz <svenhartz@users.noreply.github.com>
2012-06-25 09:53:38 +0000

    neuster stand 25.06.

--- Praktikum5/acrxEntryPoint.cpp
+++ Praktikum5/acrxEntryPoint.cpp
@@ -28,6 +28,7 @@
 #include <vector>
 #include <algorithm>
 #include <math.h>
+#include <limits>
 
 #define PI 3.14159265358979323846
 
@@ -146,18 +147,18 @@ class CPraktikum5App : public AcRxArxApp {
 			{
 				AcGePoint3d pt;
 				polyline->getPointAt(i, pt);				// gib mir den punkt im index i
-				bool exists = false;
+				/*bool exists = false;
 				for(int j = 0; j < polyPoints.size(); j++) {
 					if(polyPoints[j].x == pt.x && polyPoints[j].y == pt.y && polyPoints[j].z == pt.z) {
 						exists = true;
 					}
-				}
-				if(!exists){
+				}*/
+				//if(!exists){
 					polyPoints.push_back(pt);					// und steck ihn in polyPoints
 				//	TwoThreeTree tree;							
 				//	tree.insertAngle(360,0);					// Tree init für jeden Punkt
 				//	trees.push_back(tree);						// leg für jeden Punkt Tree mit 0°-360° an
-				}
+				//}
 			}
 
 			polyline->close();
@@ -249,11 +250,15 @@ class CPraktikum5App : public AcRxArxApp {
 		std::sort(innerEdges.begin(), innerEdges.end(), compareEdgesBySize);
 	}
 
+	// ab hier wirds spannend
 	static void createPolygons()
 	{
 		std::vector<Edge> newEdges;
 
+		// angefangen bei der kleinsten innerEdge wird nun geprüft ob das setzen erlaubt ist
 		for(int i = 0; i < innerEdges.size() && newEdges.size() < polyPoints.size() - 3; i++){	
+			
+			// wenn innerEdge[i] erlaubt ist füge sie zu newEdges hinzu
 			if(!isEdgeBlocked(innerEdges[i], polyPoints, trees)){
 				appendEdge(innerEdges[i], newEdges, polyPoints, trees);					
 			}
@@ -262,34 +267,52 @@ class CPraktikum5App : public AcRxArxApp {
 		drawPolygons(newEdges, outerEdges);
 	}
 
-	static bool isEdgeBlocked(Edge &edge, std::vector<AcGePoint3d> &polyPoints, std::vector<TwoThreeTree> &trees)
+	static bool isEdgeBlocked(Edge &innerEdge, std::vector<AcGePoint3d> &polyPoints, std::vector<TwoThreeTree> &trees)
 	{
-		for(int j = 0; j < polyPoints.size(); j++)
-		{
-			if(polyPoints[j] == edge.first){
-				int angle = (int)((atan2(edge.second.y - polyPoints[j].y,edge.second.x - polyPoints[j].x)) * 180 / PI);
+		/*
+		 * suche den anfangspunkt von innerEdge.first
+		 * und untersuche ob die innerEdge im blockingAngle des Anfangspunktes liegt
+		 */
+		for(int i = 0; i < polyPoints.size(); i++){
+			
+			if(polyPoints[i] == innerEdge.first){
+
+				// gib mir den Winkel der innenkante
+				int angle = (int)((atan2(innerEdge.second.y - polyPoints[i].y,innerEdge.second.x - polyPoints[i].x)) * 180 / PI);
 				angle < 0 ? angle += 360: angle;
 
-				int edge = trees[j].getEdgeForAngle(angle);
-				acutPrintf(_T("polyPoints[i]: %d angle: %d \n"), j, angle);
+				/* testet am anfangspunkt ob winkel für innerEdge zulässig ist
+				 * gibt 0 zurück wenn der Winkel noch frei ist
+				 */
+				int edge = trees[i].getEdgeForAngle(angle);						
 				if(edge != 0){
 					return true;
 				}
-				else {
-					return false;
+			}
+		}
+
+		// wenn Winkel noch frei kann noch ein Schnitt zwischen den Außenkanten vorliegen
+		for(int i = 0; i < outerEdges.size(); i++) {
+			if(innerEdge.first != outerEdges[i].first && innerEdge.first != outerEdges[i].second && innerEdge.second != outerEdges[i].first && innerEdge.second != outerEdges[i].second) {
+				if(hasIntersection(innerEdge, outerEdges[i])) {
+					return true;
 				}
 			}
 		}
 		return false;
 	}
 
-	static void appendEdge(Edge edge, std::vector<Edge> &edges, std::vector<AcGePoint3d> &polyPoints, std::vector<TwoThreeTree> &trees)
-	{
-		edges.push_back(edge);
-		for(int i = 0; i < trees.size(); i++)
-		{
-			if(edge.first != polyPoints[i] && edge.second != polyPoints[i])
-			{
+	static void appendEdge(Edge edge, std::vector<Edge> &newEdges, std::vector<AcGePoint3d> &polyPoints, std::vector<TwoThreeTree> &trees){
+		
+		// fügt die Kante zu newEdges hinzu
+		newEdges.push_back(edge);
+
+		/* setze im tree die blockingEdges 
+		 * wobei der Winkel von innerEdge[i] zu polyPoint[i] um 1° nach links und nach rechts reduziert wird
+		 * damit der Punkt selbst nicht fälschlicher weise auch geblockt wird
+		 */
+		for(int i = 0; i < trees.size(); i++){
+			if(edge.first != polyPoints[i] && edge.second != polyPoints[i]){
 				int angle[2];
 				angle[0] = (int)(atan2(edge.first.y - polyPoints[i].y, edge.first.x - polyPoints[i].x) * 180 / PI);
 				angle[1] = (int)(atan2(edge.second.y - polyPoints[i].y, edge.second.x - polyPoints[i].x) * 180 / PI);
@@ -298,76 +321,81 @@ class CPraktikum5App : public AcRxArxApp {
 				angle[1] < 0 ? angle[1] += 360: angle[1];
 
 				int tmp = angle[1];
-				if(angle[1] < angle[0])
+				
+				if(angle[1] < angle[0]){
 					tmp += 360;
+				}
 
 				int diff = tmp - angle[0];
-				if(diff > 180)
-				{
+				
+				if(diff > 180){
 					angle[0] -= 1;
 					angle[1] += 1;
 					angle[0] < 0 ? angle[0] += 360: angle[0];
 					angle[1] > 359 ? angle[1] -= 360: angle[1];
-					acutPrintf(_T("polyPoints[i]: %d startangle: %d endangle: %d\n"), i, angle[1], angle[0]);
-					trees[i].setBlockingEdge(angle[1], angle[0], edges.size());
-				}
-				else
-				{
+					//acutPrintf(_T("polyPoints[%d]: startangle: %d endangle: %d\n"), i, angle[1], angle[0]);
+					trees[i].setBlockingEdge(angle[1], angle[0], newEdges.size());
+				} else{
 					angle[0] += 1;
 					angle[1] -= 1;
 					angle[0] > 359 ? angle[0] -= 360: angle[0];
 					angle[1] < 0 ? angle[1] += 360: angle[1];
-					acutPrintf(_T("polyPoints[i]: %d startangle: %d endangle: %d\n"), i, angle[0], angle[1]);
-					trees[i].setBlockingEdge(angle[0], angle[1], edges.size());
+					//acutPrintf(_T("polyPoints[%d]: startangle: %d endangle: %d\n"), i, angle[0], angle[1]);
+					trees[i].setBlockingEdge(angle[0], angle[1], newEdges.size());
 				}
 			}
 		}
 	}
 
-	static void drawPolygons(std::vector<Edge> &innerEdges, std::vector<Edge> &outerEdges)
-	{
+	static void drawPolygons(std::vector<Edge> &innerEdges, std::vector<Edge> &outerEdges){
+		
+		//database connect
+		AcDbDatabase* pDB = acdbHostApplicationServices()->workingDatabase();
+
+		//blocktable init
+		AcDbBlockTable* pBlockTable = NULL;
+		pDB->getSymbolTable(pBlockTable, AcDb::kForRead);
+
+		//blocktable record init
+		AcDbBlockTableRecord* pBlockTableRecord = NULL;
+		pBlockTable->getAt(ACDB_MODEL_SPACE, pBlockTableRecord, AcDb::kForWrite);
+
+		pBlockTable->close();
+
+		/*				for(int i = 0; i < polyPoints.size(); i++)
 		{
-			//database connect
-			AcDbDatabase* pDB = acdbHostApplicationServices()->workingDatabase();
-
-			//blocktable init
-			AcDbBlockTable* pBlockTable = NULL;
-			pDB->getSymbolTable(pBlockTable, AcDb::kForRead);
-
-			//blocktable record init
-			AcDbBlockTableRecord* pBlockTableRecord = NULL;
-			pBlockTable->getAt(ACDB_MODEL_SPACE, pBlockTableRecord, AcDb::kForWrite);
-
-			pBlockTable->close();
-
-			/*				for(int i = 0; i < polyPoints.size(); i++)
-			{
-			AcDbFace* face;
-			for(int j = 0; j < newEdges.size(); j++)
-			{
-			if(polyPoints[i] == newEdges[j].first)
-			{
-			face = new AcDbFace(newEdges[j].first, newEdges[j].second, polyPoints[i == 0 ? (polyPoints.size() - 1) : i - 1], TRUE, TRUE, TRUE);
-			face->setLayer(_T("TRIANG"));
-			pBlockTableRecord->appendAcDbEntity(face);
-			face->close();
-			}
-			}
-			}
-			*/
-			AcDbLine* line;
-			for(int j = 0; j < innerEdges.size(); j++)
-			{
-				line = new AcDbLine(innerEdges[j].first, innerEdges[j].second);
-				line->setLayer(_T("TRIANG"));
-				pBlockTableRecord->appendAcDbEntity(line);
-				line->close();
-				ads_real result;
-				//acedGetReal(_T("Block"), &result);
-			}
-			pBlockTableRecord->close();
-
+		AcDbFace* face;
+		for(int j = 0; j < newEdges.size(); j++)
+		{
+		if(polyPoints[i] == newEdges[j].first)
+		{
+		face = new AcDbFace(newEdges[j].first, newEdges[j].second, polyPoints[i == 0 ? (polyPoints.size() - 1) : i - 1], TRUE, TRUE, TRUE);
+		face->setLayer(_T("TRIANG"));
+		pBlockTableRecord->appendAcDbEntity(face);
+		face->close();
 		}
+		}
+		}
+		*/
+
+		/*
+		 * TODO: statt lines faces verwenden!
+		 */
+		AcDbLine* line;
+		for(int j = 0; j < innerEdges.size(); j++)
+		{
+			line = new AcDbLine(innerEdges[j].first, innerEdges[j].second);
+			line->setLayer(_T("TRIANG"));
+			pBlockTableRecord->appendAcDbEntity(line);
+			line->close();
+			ads_real result;
+			//acedGetReal(_T("Block"), &result);
+		}
+
+
+		pBlockTableRecord->close();
+
+		
 	}
 
 	static void setOuterAngles(){
@@ -395,7 +423,7 @@ class CPraktikum5App : public AcRxArxApp {
 															
 			angle[0] < 0 ? angle[0] += 360: angle[0];		// atan2 liefert zahlen zwischen (-180) - (180)...
 			angle[1] < 0 ? angle[1] += 360: angle[1];		// ... daher +=360 wenn negativ
-
+			
 			//acutPrintf(_T("polyPoints[%d]: startangle: %d endangle: %d\n"), i, angle[0], angle[1]);
 			trees[i].setBlockingEdge(angle[0], angle[1], -1);
 		}
@@ -404,6 +432,58 @@ class CPraktikum5App : public AcRxArxApp {
 
 	static int getAngle(std::vector<AcGePoint3d> &polyPoints, int& current, int& neighbour){
 		 return (int)(atan2(polyPoints[neighbour].y - polyPoints[current].y, polyPoints[neighbour].x - polyPoints[current].x) * 180 / PI);
+	}
+
+	static bool hasIntersection(Edge &innerEdge, Edge &outerEdge){
+		
+		AcGePoint3d innerEdgeStartPoint(innerEdge.first);
+		innerEdgeStartPoint.z = 1;
+		AcGePoint3d innerEdgeEndPoint(innerEdge.second);
+		innerEdgeEndPoint.z = 1;
+
+		AcGePoint3d outerEdgeStartPoint(outerEdge.first);
+		outerEdgeStartPoint.z = 1;
+		AcGePoint3d outerEdgeEndPoint(outerEdge.second);
+		outerEdgeEndPoint.z = 1;
+
+		AcGeVector3d innerEdgeNormal = crossProduct(innerEdgeStartPoint.asVector(), innerEdgeEndPoint.asVector());
+		AcGeVector3d outerEdgeNormal = crossProduct(outerEdgeStartPoint.asVector(), outerEdgeEndPoint.asVector());
+
+		AcGeVector3d crossPoint = crossProduct(outerEdgeNormal, innerEdgeNormal);
+
+		if(crossPoint.z == 0)
+			return false;
+
+		crossPoint.x = crossPoint.x / crossPoint.z;
+		crossPoint.y = crossPoint.y / crossPoint.z;
+		crossPoint.z = 1;
+
+		ads_real t;
+		if(innerEdgeEndPoint.x - innerEdgeStartPoint.x != 0)
+			t = (crossPoint.x - innerEdgeStartPoint.x) / (innerEdgeEndPoint.x - innerEdgeStartPoint.x);
+		else if(innerEdgeEndPoint.y - innerEdgeStartPoint.y != 0)
+			t = (crossPoint.y - innerEdgeStartPoint.y) / (innerEdgeEndPoint.y - innerEdgeStartPoint.y);
+		
+		acutPrintf(_T(" t = %f\n"), t);
+		if(t > 1 || t < 0 || CompareDoubles(t, 1.0) || CompareDoubles(t, 0.0))
+			return false;
+		//else
+
+		return true;
+	}
+
+	static AcGeVector3d crossProduct(AcGeVector3d &a, AcGeVector3d &b)
+	{
+		AcGeVector3d result;
+		result.x = a.y * b.z - a.z * b.y;
+		result.y = a.z * b.x - a.x * b.z;
+		result.z = a.x * b.y - a.y * b.x;
+		return result;
+	}
+
+	static bool CompareDoubles(double a, double b) {
+	   return std::fabs(a - b) < 0.0001;
+	   //return std::fabs(a - b) < std::numeric_limits<double>::epsilon();
 	}
 } ;
 
